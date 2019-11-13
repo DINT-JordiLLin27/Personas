b3556379c26f861ddb42a6df30ce5ce21d1b02b4
Jorge <jorge.llinares@iesdoctorbalmis.com>
2019-11-13 12:40:38 +0100

    Agregar archivos de proyecto.

---
+++ WpfApp1/SexoAImagen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp1
+{
+    class SexoAImagenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value.ToString().ToLower())
+            {
+                case "hombre":
+                    return "hombre.png";
+
+                case "mujer":
+                    return "mujer.png";
+
+                default:
+                    return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
